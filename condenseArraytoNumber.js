24b4f57190fa3c371d910ec733e206651da219f1
MargaritaMarinova <47665773+MargaritaMarinova@users.noreply.github.com>
2019-05-09 16:11:28 +0300

    Add files via upload

---
+++ condenseArraytoNumber.js
@@ -0,0 +1,24 @@
+/* Write a program to receive an array of numbers and condense them by summing adjacent couples of elements 
+until a single number is obtained. For example, if we have 3 elements {2, 10, 3}, 
+we sum the first two and the second two elements and obtain {2+10, 10+3} = {12, 13}, 
+then we sum again all adjacent elements and obtain {12+13} = {25}. */
+
+function condenseArrayToNumber(arr) {
+    let condensedArr = [];
+
+    while (arr.length > 1) {
+        for (let i = 0; i < arr.length-1; i++) {
+
+            condensedArr.push(arr[i] + arr[i+1]);
+        }
+    
+        arr = condensedArr.slice();
+        condensedArr = [];
+    }
+
+    console.log(arr[0]);
+
+
+}
+
+condenseArrayToNumber([2,10,3]);
