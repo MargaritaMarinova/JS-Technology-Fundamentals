7e724cd982ca81e0048d1de28ca9437cfc3f125f
Margarita Tomova-Marinova <margarita.tomova@gmail.com>
2019-05-10 12:21:56 +0300

    Code optimisation

--- Arrays/equalArrays.js
+++ Arrays/equalArrays.js
@@ -8,16 +8,9 @@ differ and print on the console following message:
  */
 
 function equalArrays(arr1, arr2) {
-    // for (let i = 0; i < arr1.length; i++) {
-    //     arr1[i] = Number(arr1[i]);
-    // }
     convertToNumber(arr1);
     convertToNumber(arr2);
-
-    // for (let j=0; j< arr2.length; j++) {
-    //     arr2[j] = Number(arr2[j]);
-    // }
-
+    
     let areEqual = true;
     let sum = 0;
 
@@ -44,5 +37,6 @@ function equalArrays(arr1, arr2) {
 
 }
 
+//test the result
 equalArrays(['1','2','3','4','5'], ['1','2','4','4','5']);
 
